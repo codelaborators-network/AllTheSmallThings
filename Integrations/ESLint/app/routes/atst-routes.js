4e0cd79915d095bdc84ec3de9ec0c40f74efbf0c
Zac Braddy <zacharybraddy@gmail.com>
2017-03-18 17:46:19 +0000

    Removed ESLint integration to push it to it's own repo

--- Integrations/ESLint/app/routes/atst-routes.js
+++
@@ -1,9 +0,0 @@
-const doLint = require('../BL/doLint');
-
-module.exports = function(app) {
-    app.post('/lint', (req, res) => {
-        doLint(req.body);
-
-        res.sendStatus(200);
-    });
-};
