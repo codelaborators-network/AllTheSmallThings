7571fb7b9bb46a18d708626d72cf8d21390f797b
James Studdart <james@studdart.co.uk>
2017-03-19 04:32:09 +0000

    started adding some stats for users

---
+++ Api/atst.Api/ApiRole/Modules/Stats/StatsModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using atst.Core.Authentication.Entities;
+using atst.Core.Helpers;
+using atst.Core.Stats.Entities;
+using Nancy;
+
+namespace ApiRole.Modules.Stats
+{
+    public class StatsModule : NancyModule
+    {
+        private readonly IFirebaseHelper _firebaseHelper;
+        public StatsModule(IFirebaseHelper firebaseHelper) : base("/api/stats/")
+        {
+            _firebaseHelper = firebaseHelper;
+            Post["/users/"] = _ => GetUserDetails();
+            Post["/users/{userName}"] = _ => GetUserDetails(_.userName);
+        }
+
+        private dynamic GetUserDetails()
+        {
+            var users = new List<User>();
+
+            return Response.AsJson(users);
+        }
+
+        private dynamic GetUserDetails(string userName)
+        {
+            var user = (ExtendedUser) _firebaseHelper.GetUserAsync(userName).Result;
+            return Response.AsJson(user);
+        }
+    }
+}
---
+++ Core/atst.Core/atst.Core/Stats/Entities/ExtendedUser.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using atst.Core.Authentication.Entities;
+
+namespace atst.Core.Stats.Entities
+{
+    public class ExtendedUser : User
+    {
+        public Dictionary<DateTime, int> TodaysCheckins
+        {
+            get
+            {
+                return XpHistory.Where(x => x.DateTime >= DateTime.UtcNow.Date).ToDictionary(x => x.DateTime, x => x.Value);
+            }
+        }
+
+        public int XpForToday
+        {
+            get
+            {
+                return TodaysCheckins.Sum(x => x.Value);
+            }
+        }
+
+        public Dictionary<DateTime, int> YesterdaysCheckins
+        {
+            get
+            {
+                return XpHistory.Where(x => x.DateTime >= DateTime.UtcNow.Date.AddDays(-1) && x.DateTime < DateTime.UtcNow.Date).ToDictionary(x => x.DateTime, x => x.Value);
+            }
+        }
+
+        public int XpForYesterday
+        {
+            get
+            {
+                return YesterdaysCheckins.Sum(x => x.Value);
+            }
+        }
+
+        public double AverageCheckinsPerHourForYesterday
+        {
+            get
+            {
+                var firstCheckin = YesterdaysCheckins.OrderByDescending(x => x.Key).Select(x => x.Key).FirstOrDefault();
+                var lastCheckin = YesterdaysCheckins.OrderByDescending(x => x.Key).Select(x => x.Key).LastOrDefault();
+
+                if (firstCheckin != null && lastCheckin != null && firstCheckin != lastCheckin)
+                {
+                    return YesterdaysCheckins.Count / (firstCheckin - lastCheckin).TotalHours;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+    }
+}
