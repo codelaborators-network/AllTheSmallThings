937213d514def0182e3637fb42bb8449f5e6b764
Jamie Taylor <seppydude@hotmail.com>
2017-03-19 10:53:24 +0000

    Individual User page (not complete)

--- Integrations/asti.CustomerServiceApplication/asti.CustomerServiceApplication/Models/UserClass.cs
+++ Integrations/asti.CustomerServiceApplication/asti.CustomerServiceApplication/Models/UserClass.cs
@@ -19,12 +19,33 @@ namespace asti.CustomerServiceApplication.Models
 
       [JsonProperty("xpHistory")]
       public List<XpHistory> XpHistory { get; set; }
+
+      [JsonProperty("todaysCheckins")]
+      public List<DateBasedCheckin> TodaysCheckins { get; set; }
+
+      [JsonProperty("yesterdaysCheckins")]
+      public List<DateBasedCheckin> YesterdaysCheckins { get; set; }
    }
 
    public class XpHistory
    {
       [JsonProperty("value")]
       public int Value { get; set; }
+
+      [JsonProperty("dateTime")]
+      public DateTime DateTime { get; set; }
+
+      [JsonProperty("ActionType")]
+      public int ActionType { get; set; }
+   }
+
+   public class DateBasedCheckin
+   {
+      [JsonProperty("value")]
+      public int Value { get; set; }
+
+      [JsonProperty("actionType")]
+      public int ActionType { get; set; }
 
       [JsonProperty("dateTime")]
       public DateTime DateTime { get; set; }
