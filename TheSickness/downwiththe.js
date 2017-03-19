8caaf64cd26155aa6fe39c838006505b12acbf8e
Zac Braddy <zacharybraddy@gmail.com>
2017-03-19 07:03:41 +0000

    Testing framework

--- TheSickness/downwiththe.js
+++ TheSickness/downwiththe.js
@@ -5,5 +5,5 @@ module.exports = function(youMotherGetUp) {
         return getDownWith;
     }
 
-     return new Array(16).join('hero'-1) + "Batman"
+     return new Array(16).join('hero'-1) + "Batman";
 };
